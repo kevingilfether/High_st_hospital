a26bd3ca260f175173cd5eb9e7d17da56cd15127
kevingilfether <kevingilfether@gmail.com>
2017-10-12 15:31:40 -0400

    Added switch and lists for classes

--- High_st_hospital/Program.cs
+++ High_st_hospital/Program.cs
@@ -10,6 +10,13 @@ namespace High_st_hospital
     {
         static void Main(string[] args)
         {
+            //Setting up lists of employees
+            List<Employee> employeeList = new List<Employee>();
+            List<Doctor> doctorList = new List<Doctor>();
+            List<Surgeon> surgeonList = new List<Surgeon>();
+            List<Nurse> nurseList = new List<Nurse>();
+            List<Receptionist> receptionistList = new List<Receptionist>();
+
             //copy for hospital management system
             Console.WriteLine("Welcome to High St. Hospital Personnel Managment System.");
             Console.WriteLine("(HSHPMS");
@@ -19,11 +26,11 @@ namespace High_st_hospital
             Console.WriteLine("LOOKUP or ADD");
             string action = Console.ReadLine().Trim().ToUpper();
 
-            bool isDoneAdding;
+            bool isDoneAdding = true;
             if (action == "ADD")
                 isDoneAdding = false; 
 
-            while (isDoneAdding == false)
+            while (!isDoneAdding)
             {
                 Console.WriteLine("Add an Employee");
                 Console.WriteLine("What kind of employee do you want to add?");
@@ -38,17 +45,20 @@ namespace High_st_hospital
                         int employeeNum = int.Parse(Console.ReadLine());
                         Console.WriteLine("What is the doctor's area of specialty?");
                         string specialtyArea = Console.ReadLine();
-                        Console.WriteLine("What is the ")
-
+                        doctorList.Add(new Doctor(employeeName, employeeNum, specialtyArea));
+                        break;
 
                     case "SURGEON":
+                        break;
 
                     case "NURSE":
+                        break;
 
                     case "RECEPTIONIST":
+                        break;
 
-                    case default:
-                    
+                    default:
+                        break;
 
                 }
             }
