6d544477a4f70e77469d9c3cee594a57f1555c0a
kevingilfether <kevingilfether@gmail.com>
2017-10-12 13:12:42 -0400

    Initial Commit

---
+++ High_st_hospital/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace High_st_hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
